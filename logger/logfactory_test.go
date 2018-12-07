bbf9a9093ddbaadec7c27e830ce1b9bc81eecc66
Tracy Li <tibcotracy@gmail.com>
2018-12-06 18:45:26 -0600

    Remove fmt.print  to use fatal while there is panic

--- logger/logfactory_test.go
+++ logger/logfactory_test.go
@@ -23,7 +23,7 @@ func TestConcurrentGetLoggerOk(t *testing.T) {
 			defer func() {
 				if r := recover(); r != nil {
 					recovered = r
-					fmt.Println(recovered)
+					t.Fatal(recovered)
 				}
 			}()
 			loggerName := fmt.Sprintf("logger%f", math.Mod(float64(i), 5))
