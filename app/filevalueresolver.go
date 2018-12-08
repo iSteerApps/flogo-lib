864be248266af4d83d8cf99a48a6c36dc7b93575
Vijay Nalawade <vnalawad@tibco.com>
2018-12-07 16:41:00 -0800

    Code refactoring

--- app/filevalueresolver.go
+++ app/filevalueresolver.go
@@ -25,31 +25,25 @@ func init() {
 		// Register value resolver
 		RegisterPropertyValueResolver("file", &FileValueResolver{})
 
-		//load props from external files
 		if config.GetAppPropertiesValueResolver() == "" {
 			//Make file resolver default since FLOGO_APP_PROPS_FILE_CONFIG is set
 			os.Setenv(config.ENV_APP_PROPERTY_RESOLVER_KEY, "file")
 		}
 
-		// preload to props from files
+		// preload props from files
 		files := strings.Split(filePaths, ",")
 		if len(files) > 0 {
 			for _, filePath := range files {
 				props := make(map[string]interface{})
-				if strings.HasSuffix(filePath, ".json") {
-					// Override through file
-					file, e := ioutil.ReadFile(filePath)
-					if e != nil {
-						log.Errorf("Can not read - %s due to error - %v", filePath, e)
-						panic("")
-					}
-					e = json.Unmarshal(file, &props)
-					if e != nil {
-						log.Errorf("Can not read - %s due to error - %v", filePath, e)
-						panic("")
-					}
-				} else {
-					log.Error("File must be a valid JOSN e.g. props.json")
+
+				file, e := ioutil.ReadFile(filePath)
+				if e != nil {
+					log.Errorf("Can not read - %s due to error - %v", filePath, e)
+					panic("")
+				}
+				e = json.Unmarshal(file, &props)
+				if e != nil {
+					log.Errorf("Can not read - %s due to error - %v", filePath, e)
 					panic("")
 				}
 
